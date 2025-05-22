260cb9d0f6eba59410ed24aa623b7a33d26bdac2
HASNAIN-ALI-MUHAMMAD <hasali9349@gmail.com>
2025-05-23 01:11:20 +0500

    trying again4@worker pdf-dist

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode:true,
+    experimental:{
+        outputFileTracingIncludes:{
+            'api/process/pdf':[
+                 './node_modules/pdfjs-dist/build/pdf.worker.js',
+                 './node_modules/pdfjs-dist/package.json',
+            ],
+        }
+    },
      webpack: (config, { isServer }) => {
     if (isServer) {
       if (!config.externals) {
